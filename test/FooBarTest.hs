cae267f4dba33e3f92d797c5907782edb748ea8d
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-03-04 11:04:48 +0100

    better QuickCheck properties

--- test/FooBarTest.hs
+++ test/FooBarTest.hs
@@ -3,8 +3,10 @@ module Main (main) where
 import FooBar (fooBar)
 import Test.QuickCheck
 
-prop_fooBar :: String -> Bool
-prop_fooBar x = fooBar x == "Hello " ++ x
+prop_fooBar :: Property
+prop_fooBar = property $ do
+  x <- listOf1 (chooseAny :: Gen Char)
+  return $ fooBar x === "Hello " ++ x
 
 main :: IO ()
 main = do
